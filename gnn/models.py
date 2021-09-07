92c8c27a3ce518d5dc00d843e58153240ef44766
HiddenBeginner <mostp______@naver.com>
2021-09-07 11:47:31 +0900

    Make more general model architecture

--- gnn/models.py
+++ gnn/models.py
@@ -12,10 +12,67 @@ class GCN(torch.nn.Module):
     ----------
     https://colab.research.google.com/drive/1I8a0DfQ3fI7Njc62__mVXUlcAleUclnb?usp=sharing
     """
-    def __init__(self, embedding_dim, hidden_channels, hidden_dims, num_node_features=13, dropout=0.5):
+    def __init__(self, embedding_dim, hidden_channels, hidden_dims, num_node_features=13):
         super(GCN, self).__init__()
-        self.embedding = nn.Embedding(num_node_features, embedding_dim) if embedding_dim is not None else None
-        embedding_dim = num_node_features
+        if embedding_dim is not None:
+            self.embedding = nn.Embedding(num_node_features, embedding_dim)
+        else:
+            self.embedding = None
+            embedding_dim = num_node_features
+        
+        # GNN
+        self.conv_layers = nn.ModuleList()
+        for i in range(len(hidden_channels)):
+            if i == 0:
+                conv = GCNConv(embedding_dim, hidden_channels[i], normalize=False)
+            else:
+                conv = GCNConv(hidden_channels[i - 1], hidden_channels[i], normalize=False)
+            self.conv_layers.append(conv)
+
+        # MLP
+        self.fc_layers = nn.ModuleList()
+        for i in range(len(hidden_dims)):
+            if i == 0:
+                fc = nn.Linear(hidden_channels[-1], hidden_dims[i])
+            else:
+                fc = nn.Linear(hidden_dims[i - 1], hidden_dims[i])
+            self.fc_layers.append(fc)
+        self.out = nn.Linear(hidden_dims[-1], 1)
+        
+    def forward(self, x, edge_index, batch):
+        if self.embedding is not None: 
+            x = torch.nonzero(x, as_tuple=True)[1]
+            x = self.embedding(x)
+
+        # 1. Obtain node embeddings through gnn
+        for i, conv in enumerate(self.conv_layers):
+            x = F.relu(conv(x, edge_index))
+            x = F.normalize(x, 2, 1)
+        
+        # 2. graph pooling
+        x = global_mean_pool(x, batch)  # [batch_size, hidden_channels]
+        
+        # 3. Downstream mlp
+        for i, fc in enumerate(self.fc_layers):
+            x = F.relu(fc(x))        
+        x = self.out(x)
+        
+        return x
+ 
+
+class SkipConnectionGCN(torch.nn.Module):
+    """
+    references
+    ----------
+    https://colab.research.google.com/drive/1I8a0DfQ3fI7Njc62__mVXUlcAleUclnb?usp=sharing
+    """
+    def __init__(self, embedding_dim, hidden_channels, hidden_dims, num_node_features=13):
+        super(SkipConnectionGCN, self).__init__()
+        if embedding_dim is not None:
+            self.embedding = nn.Embedding(num_node_features, embedding_dim)
+        else:
+            self.embedding = None
+            embedding_dim = num_node_features
         
         # GNN
         self.conv_layers = nn.ModuleList()
@@ -35,8 +92,6 @@ class GCN(torch.nn.Module):
                 fc = nn.Linear(hidden_dims[i - 1], hidden_dims[i])
             self.fc_layers.append(fc)
         self.out = nn.Linear(hidden_dims[-1], 1)
-
-        self.dropout = dropout
         
     def forward(self, x, edge_index, batch):
         if self.embedding is not None: 
@@ -45,18 +100,18 @@ class GCN(torch.nn.Module):
 
         # 1. Obtain node embeddings through gnn
         for i, conv in enumerate(self.conv_layers):
-            x = conv(x, edge_index)
+            skip_connection = x
+            x = conv(x, edge_index) + skip_connection
             if i != len(self.conv_layers) - 1:
-                x = x.relu()
+                x = F.relu(x)
         
         # 2. graph pooling
         x = global_mean_pool(x, batch)  # [batch_size, hidden_channels]
         
         # 3. Downstream mlp
         for fc in self.fc_layers:
-            # x = F.dropout(x, p=self.dropout, training=self.training)
             x = F.relu(fc(x))
         x = self.out(x)
         
         return x
- 
+  
